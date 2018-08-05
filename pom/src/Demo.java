32a408bb3ffcad666694a38c5636191edd70aa52
Vinay Konda <Vinay Konda@VINAY>
2018-08-05 17:19:24 +0530

    first commit

---
+++ pom/src/Demo.java
@@ -0,0 +1,19 @@
+
+public class Demo {
+
+	public void main (String[] args) {
+		
+		String[] str = new String[3];
+		str[0]="vinay";
+		
+		str[1]="konda";
+		str[2]="kumar";
+		
+		for(int i=0;i<str.length;i++) {
+			System.out.println(str[i]);
+		}
+	}
+	
+	
+}
+ 
---
+++ pom/src/com/Marks.java
@@ -0,0 +1,34 @@
+package com;
+
+import java.util.ArrayList;
+
+public class Marks {
+private double physicsMarks;
+private double ChemistryMarks;
+private double mathsMarks;
+public double getPhysicsMarks() {
+	return physicsMarks;
+}
+public void setPhysicsMarks(double physicsMarks) {
+	this.physicsMarks = physicsMarks;
+}
+public double getChemistryMarks() {
+	return ChemistryMarks;
+}
+public void setChemistryMarks(double chemistryMarks) {
+	ChemistryMarks = chemistryMarks;
+}
+public double getMathsMarks() {
+	return mathsMarks;
+}
+public void setMathsMarks(double mathsMarks) {
+	this.mathsMarks = mathsMarks;
+}
+public Marks (double physicsMarks, double ChemistryMarks,double mathsMarks) {
+    this. physicsMarks = physicsMarks;
+    this.ChemistryMarks = ChemistryMarks;
+    this.mathsMarks = mathsMarks;
+    
+
+}
+}
